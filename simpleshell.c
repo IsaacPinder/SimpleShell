c543697c5cae3c7b071f12c2183f78d7ff792977
Isaac Pinder <isaac.pinder.2023@uni.strath.ac.uk>
2025-01-30 13:09:23 +0000

    Cleaned up code and added commentary - Isaac,Scott

--- simpleshell.c
+++ simpleshell.c
@@ -1,74 +1,94 @@
-# include <stdio.h>
-# include <string.h>
- 
-int str_len(char* str){
-int valid = 0;
-int count = 0;
-while (!valid){
-if (*str == '\0'){
-valid = 1;
-}
-else{
-count ++;
-str ++;
-}
-}
-return count;
-}
+#include <stdio.h>
+#include <string.h>
+// ** Commands for compiling and running **
+// gcc simpleshell.c -pedantic -Wall -o simpleshell
+/// simpleshell
 
- int str_cmp(char *str1, char *str2){
-for(int i = 0; i < str_len(str1);i++){
-
-if (*(str1 + i) > *(str2 + i)){
-return 1;
-}
-
-if (*(str1 + i) < *(str2 + i)){
-return -1;
-}
+// Coded by Isaac during Semester 1 excercises
+// Function takes  pointer to string and returns int length
+int str_len(char *str)
+{
+  int valid = 0;
+  int count = 0;
+  while (!valid)
+  {
+    if (*str == '\0')
+    {
+      valid = 1;
+    }
+    else
+    {
+      count++;
+      str++;
+    }
+  }
+  return count;
 }
 
-return 0;
+// Coded by Isaac during Semester 1 excercises
+// function takes 2 pointers to 2 strings and returns 0 if they are equal
+int str_cmp(char *str1, char *str2)
+{
+  for (int i = 0; i < str_len(str1); i++)
+  {
 
+    if (*(str1 + i) > *(str2 + i))
+    {
+      return 1;
+    }
+
+    if (*(str1 + i) < *(str2 + i))
+    {
+      return -1;
+    }
+  }
+
+  return 0;
 }
 
+// Coded together as a full group
+// Prompt function prints £: and takes user input
+// upon "exit" or ^D being entered shell will quit
 
-
-
-
- void prompt(){
+void prompt()
+{
+  // int exit to for use in main loop to determine if we should continue running
   int exit = 0;
+  // pointer to string for use with str_cmp to compare input to "exit"
   char *exitstr = "exit\n";
 
-    while(exit == 0){
-      printf("£: ");
-      
-      char input[512];
-      
-     char* line = fgets(input,sizeof(input),stdin);
-    
-      if(str_cmp(input,exitstr) == 0||line == NULL){
-        exit = 1;
-        
-      }
-      
-     char* token = strtok(line," \t|><&;");
-      
-      while(token != NULL){
-      printf("%s ",token);
-      token = strtok(NULL," \t|><&;");
-      }
-      
+  // main loop continue while exit != 1
+  while (exit == 0)
+  {
+    printf("£: ");
+    // buffer to take in input
+    char input[512];
+    // take in user input
+    char *line = fgets(input, sizeof(input), stdin);
+
+    // if User input is "exit" or ^D is pressed(therefor line is NULL)then done looping
+    if (str_cmp(input, exitstr) == 0 || line == NULL)
+    {
+      exit = 1;
     }
-   printf("yay u left\n");
- }
- 
- 
- 
- 
- 
- int main(void) {
-   prompt();
-   return 0;
+
+    // tolkenises the line string by provided delimeters e.g (" \t|><&;")
+    char *token = strtok(line, " \t|><&;");
+
+    // loops through line string breaking it into smaller strings until end of line
+    while (token != NULL)
+    {
+      // this print f causes issue with spacing of second £: onwards
+      printf("%s ", token);
+      token = strtok(NULL, " \t|><&;");
+    }
+  }
+  // exit confirmation message
+  printf("yay u left\n");
 }
- 
+
+int main(void)
+{
+  prompt();
+  return 0;
+}

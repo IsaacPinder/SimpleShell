a02733fcd1d193ac42e7c657699ffed3970cc55f
Isaac Pinder SE2023 <isaac.pinder.2023@uni.strath.ac.uk>
2025-02-04 11:39:59 +0000

    Update file helpers.c

--- helpers.c
+++ helpers.c
@@ -43,3 +43,45 @@ int str_cmp(char *str1, char *str2)
 
   return 0;
 }
+// Coded by Isaac during Semester 1 excercises 
+// for use in str_trim
+int whitespace(char c){
+ return ( c ==' ' || c =='\t');
+}
+
+
+// Coded by Isaac during Semester 1 excercises 
+// for use in str_trim, iterates through string, removes whitespace charcters as defined in whitespace function
+char *str_trim(char *str){
+int startpos = 0;
+int endpos = str_len(str) - 1;
+
+for(int i = 0; i < str_len(str);i++){
+
+if (!whitespace(*(str + i))){
+startpos = i;
+break;
+}
+}
+
+
+for(int j = endpos; j >= 0; j--){
+if (!whitespace(*(str + j))) {
+            endpos = j;
+            break; 
+}
+}
+
+
+
+if (startpos > endpos) {
+        *str = '\0'; 
+        return str;
+    }
+
+
+*(str + endpos + 1) = '\0';
+char *newpoint = str + startpos;
+
+return newpoint;
+}
